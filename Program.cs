ab97a0e44454184d1ea5c3fdd4aeafdd8bb27be6
Kyle Zamora <58868153+kjzamora@users.noreply.github.com>
2021-12-19 20:17:03 -0500

    Added Data Library, Data Access IDataAccess, and UserModel

--- Program.cs
+++ Program.cs
@@ -1,15 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using ATM.Models;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System.Threading.Tasks;
 
 namespace ATM
 {
-    class Program : SystemMessaging
+    class Program
     {
         static void Main(string[] args)
         {
+
+            IDataAccess _data = new DataAccess();
+            List<UserModel> user;
+            string sql = "select * from tbluseraccount";
+            string MySqlBaseConnectionStringBuilder = @"server=localhost; database=usersdb; user id=DBAdmin; password=admin;";
+
+            try
+            {
+                user = _data.LoadData<UserModel, dynamic>(sql, new { }, MySqlBaseConnectionStringBuilder);
+                foreach (var acc in user)
+                {
+                    Console.WriteLine(acc.UserName);
+                }
+            }
+            
+            catch(MySqlException err)
+            {
+                Console.Write(err);
+            }
+
+            Console.Read();
+
+
+
+        //string str = @"server=localhost;database=usersdb;user id=DBAdmin;password=admin;";
+        //    MySqlConnection con = null;
+        //    MySqlDataReader reader = null;
+        //    try
+        //    {
+        //        con = new MySqlConnection(str);
+        //        con.Open();
+        //        Console.WriteLine("MySQL DB Connected");
+        //        String cmdText = "SELECT * FROM tbluseraccount";
+        //        MySqlCommand cmd = new MySqlCommand(cmdText, con);
+        //        reader = cmd.ExecuteReader();
+                
+        //        while (reader.Read())
+        //        {
+        //            Console.WriteLine(reader.GetString(0));
+        //        }
+        //    }
+
+        //    catch(MySqlException err)
+        //    {
+        //        Console.Write(err);
+        //    }
+
+        //    finally
+        //    {
+        //        if (con != null)
+        //        {
+        //            con.Close();
+        //        }
+        //    }
+
+        //    Console.Read();
+
             SystemMessaging.WelcomeMessage();
 
             Console.Write("User:  ");
-            string user = Console.ReadLine();
+            string user1 = Console.ReadLine();
 
             Console.Write("Password:  ");
             string password = Console.ReadLine();
---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ATM
+{
+    class Startup
+    {
+        //public Startup(Configuration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+
+        //public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Establish one connection to MySQL
+            services.AddSingleton<IDataAccess, DataAccess>();
+        }
+    }
+}
